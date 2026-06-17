9142156d3fb767fbb65fa1a66f3cadfcb6025661
MidAutumnMoon <me@418.im>
2026-06-17 21:24:20 +0800

    add JS-Slash-Runner and use its types

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="./vendor/SillyTavern/public/global.d.ts" />
+/// <reference types="./vendor/JS-Slash-Runner/@types/function/index.d.ts" />
